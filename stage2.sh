d6e4dad99d24cb8352bff453d0166d547b32c333
Peter Mamonov <pmamonov@gmail.com>
2018-06-09 02:14:36 +0300

    fixup! install personal stuff

--- stage2.sh
+++ stage2.sh
@@ -7,7 +7,7 @@ apt-get -y purge systemd
 apt-get -y install						\
 	net-tools openssh-client openssh-server ntp		\
 	vim make gcc gdb less					\
-	slim i3 xscreensaver firefox-esr mutt gnupg		\
+	slim i3 xserver-xorg xscreensaver firefox-esr mutt gnupg	\
 	rxvt-unicode screen minicom ipython			\
 	firmware-realtek wicd-curses				\
 	sysvinit-core						\
