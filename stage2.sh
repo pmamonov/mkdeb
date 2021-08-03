26578e3f2b3b312be3b5644209a119bce055c2d9
Peter Mamonov <pmamonov@gmail.com>
2021-08-03 18:14:03 +0300

    stage2.sh: remove X stuff

--- stage2.sh
+++ stage2.sh
@@ -5,7 +5,6 @@ apt-get update || exit $?
 apt-get -y --force-yes install					\
 	net-tools openssh-client openssh-server ntp		\
 	vim make gcc gdb less					\
-	slim xfce4 firefox-esr					\
 || exit $?
 
 passwd
